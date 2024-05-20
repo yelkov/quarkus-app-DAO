f40ae50c49447eab242b5119c891958db4309bb1
yelkov <yelkovq@gmail.com>
2024-05-20 15:03:53 +0200

    feat(Item.java): mapear clase Item

---
+++ src/main/java/edu/badpals/quarkusapp/Item.java
@@ -0,0 +1,59 @@
+package edu.badpals.quarkusapp;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="t_items")
+public class Item extends PanacheEntityBase {
+    public Item() {
+    }
+
+    public Item(String nombre, Integer quality) {
+        this.nombre = nombre;
+        this.quality = quality;
+    }
+
+    public Item(String nombre, Integer quality, String tipo) {
+        this.nombre = nombre;
+        this.quality = quality;
+        this.tipo = tipo;
+    }
+
+    @Id
+    @Column(name="item_nom")
+    private String nombre = "";
+
+    @Column(name="item_prop")
+    private Integer quality = 0;
+
+    @Column(name="item_tipo")
+    private String tipo = "";
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getQuality() {
+        return quality;
+    }
+
+    public void setQuality(Integer quality) {
+        this.quality = quality;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
--- src/test/java/edu/badpals/quarkusapp/ServiceTest.java
+++ src/test/java/edu/badpals/quarkusapp/ServiceTest.java
@@ -17,8 +17,8 @@ public class ServiceTest {
     @PersistenceContext
     jakarta.persistence.EntityManager em;
 
-    @Inject
-    ServiceOlli servicio;
+    /*@Inject
+    ServiceOlli servicio;*/
 
     /**
 	 * MAPPINGS de la entidades a las tablas de la BBDD.
@@ -32,14 +32,14 @@ public class ServiceTest {
 	 * Completa la definicion y el mapping
 	 * de la clase Item a la tabla t_items
 	 */
-    /*@Test
+    @Test
     public void test_mapping_normalItem() {
         Item elixir = em.find(Item.class, "Elixir of the Mongoose");
         Assertions.assertThat(elixir).isNotNull();
         Assertions.assertThat(elixir.getNombre()).isEqualTo("Elixir of the Mongoose");
 		Assertions.assertThat(elixir.getQuality()).isEqualTo(7);
         Assertions.assertThat(elixir.getTipo()).isEqualTo("NormalItem");
-    }*/
+    }
 
 	/**
 	 * Completa la definicion y el mapping
