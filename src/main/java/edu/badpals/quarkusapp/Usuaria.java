cb7a083ef1fd7cacf8d0c26a45c3bca63583d58d
yelkov <yelkovq@gmail.com>
2024-05-20 15:08:06 +0200

    feat(Usuaria.java): mapear clase Usuaria

---
+++ src/main/java/edu/badpals/quarkusapp/Usuaria.java
@@ -0,0 +1,55 @@
+package edu.badpals.quarkusapp;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="t_users")
+public class Usuaria extends PanacheEntityBase {
+
+    public Usuaria() {
+    }
+
+    public Usuaria(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Usuaria(String nombre, Integer destreza) {
+        this.nombre = nombre;
+        this.destreza = destreza;
+    }
+
+    @Id
+    @Column(name="user_nom")
+    private String nombre = "";
+
+    @Column(name="user_prop")
+    private Integer destreza = 0;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getDestreza() {
+        return destreza;
+    }
+
+    public void setDestreza(Integer destreza) {
+        this.destreza = destreza;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuaria{" +
+                "nombre='" + nombre + '\'' +
+                ", destreza=" + destreza +
+                '}';
+    }
+}
--- src/test/java/edu/badpals/quarkusapp/ServiceTest.java
+++ src/test/java/edu/badpals/quarkusapp/ServiceTest.java
@@ -45,13 +45,13 @@ public class ServiceTest {
 	 * Completa la definicion y el mapping
 	 * de la clase Usuaria a la tabla t_users
 	 */
-	/*@Test
+	@Test
 	public void test_mapping_usuaria() {
 		Usuaria elfo = em.find(Usuaria.class, "Doobey");
         Assertions.assertThat(elfo).isNotNull();
         Assertions.assertThat(elfo.getNombre()).isEqualTo("Doobey");
         Assertions.assertThat(elfo.getDestreza()).isEqualTo(15);
-	}*/
+	}
 
     /**
 	 * Completa la definicion y el mapping
